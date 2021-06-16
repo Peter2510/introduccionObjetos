3135c1e7ecc5be681ef5a35985c7d7fcf454b55b
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-16 13:46:04 -0600

    metodo Calcular peso ideal

--- src/Persona.java
+++ src/Persona.java
@@ -6,12 +6,26 @@ public class Persona {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Double radio;
 
+        System.out.println("\n");
+
+        System.out.print("\nIngrese el radio del circulo: ");
+        radio = scanner.nextDouble();
+
+        // Creando el objeto cuadrado
+        Circulo circulo = new Circulo(radio);
+
+        circulo.calcularArea();
+        circulo.calcularPerimetro();
+
+        System.out.println("\nEl area del circulo de radio " + circulo.getRadio() + " es de: " + circulo.getArea() + " unidades, y el perimetro es: " + circulo.getPerimetro());
 
     }
 
     private String nombre, edad, sexo = "Hombre";
-    private int dni,peso altura;
+    private int dni,peso altura, resultadoPeso;
 
     // constructor de la clase
 
@@ -42,6 +56,29 @@ public class Persona {
     }
 
 
-   
+    // calcular peso ideal
+    public void calcularIMC() {
+
+        int pesoIdeal = (peso)/(altura*altura);
+
+        if (pesoIdeal < 20) {
+
+            resultadoPeso = -1;
+            
+        }
+
+        if (pesoIdeal >= 20 && pesoIdeal <=25) {
+
+            resultadoPeso =0;
+            
+        }
+
+        if (pesoIdeal>25) {
+
+            resultadoPeso = 1;
+            
+        }
+
+    
 
 }
