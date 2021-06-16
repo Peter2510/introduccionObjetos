de77a400e802e9d9f06c9ed2acb3bfb22f9c8f77
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-16 01:39:24 -0600

    Cree el archivo del ejercicio 1, contador y realice la codificacion

---
+++ src/Contador.java
@@ -0,0 +1,113 @@
+package src;
+
+import java.util.*;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        Contador contador = new Contador();
+
+        int opcion = 0;
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("\n");
+        System.out.print("\nEl contador inicia en 0");
+
+        do {
+
+        System.out.print("\n");
+
+
+            contador.mostrarValor();
+            contador.mostrarmenu();
+
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+
+                contador.aumentarContador();
+
+            }
+
+            if (opcion == 2) {
+
+                contador.decrementarContador();
+
+            }
+
+        } while (opcion != 3);
+        {
+            System.out.println("Hasta pronto");
+        }
+
+    }
+
+    private int contadorVariable;
+    
+
+    // construcor vacio
+    public Contador() {
+
+    }
+
+    //construcctor con parametros
+    public Contador(int contador) {
+
+        this.contadorVariable = contador;
+
+    }
+
+    public void mostrarmenu(){
+
+
+            System.out.println("\nSelecciona una opcion");
+            System.out.println("\n1. Aumentar contador");
+            System.out.println("\n2. Decrementar contador");
+            System.out.println("\n3. Salir");
+            System.out.print("\nSelecciona una opcion: ");
+
+
+
+
+    }
+
+    public void aumentarContador() {
+
+        contadorVariable++;
+        setContador(contadorVariable);
+
+    }
+
+    public void decrementarContador() {
+
+        contadorVariable--;
+
+        setContador(contadorVariable);
+    }
+
+    public int getContador() {
+
+        return contadorVariable;
+
+    }
+
+    public void setContador(int contador) {
+
+        this.contadorVariable = contador;
+
+    }
+
+    public void mostrarValor() {
+
+        
+
+        System.out.println("----------------------------------------------");
+
+        System.out.println("\nValor actual del contador: " + getContador());
+
+    }
+
+   
+}
