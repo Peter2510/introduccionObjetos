06d5780234195222b0b8d93795bd8b9fdbc35684
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-16 15:00:59 -0600

    Ejercicio Bono, pedir datos

--- src/Persona.java
+++ src/Persona.java
@@ -7,25 +7,40 @@ public class Persona {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        Double radio;
-
+        int altura, edad, peso;
+        String nombre, sexo;
+        
         System.out.println("\n");
 
-        System.out.print("\nIngrese el radio del circulo: ");
-        radio = scanner.nextDouble();
+        System.out.print("\nIngresa el nombre de la persona: ");
+        nombre = scanner.nextLine();
 
-        // Creando el objeto cuadrado
-        Circulo circulo = new Circulo(radio);
+        System.out.print("\nIngresa la edad de la persona: ");
+        edad = scanner.nextInt();
 
-        circulo.calcularArea();
-        circulo.calcularPerimetro();
+        System.out.println("Ingresa el peso");
+        peso = scanner.nextInt();
 
-        System.out.println("\nEl area del circulo de radio " + circulo.getRadio() + " es de: " + circulo.getArea() + " unidades, y el perimetro es: " + circulo.getPerimetro());
+        System.out.println("Ingrese la altura");
+        altura = scanner.nextInt();
 
+        System.out.println("Ingresa f si es mujer o h si es hombre");
+        sexo = scanner.nextLine();
+        sexo = scanner.nextLine();
+        
+        Persona p1 = new Persona(nombre, edad, sexo, peso, altura);
+
+       
+
+
+
+         
     }
 
-    private String nombre, edad, sexo = "Hombre";
-    private int dni,peso altura, resultadoPeso;
+    private String nombre, sexo;
+    private int dni,peso,edad, altura, resultadoPeso;
+    public boolean mayorEdad;
+    
 
     // constructor de la clase
 
@@ -44,41 +59,98 @@ public class Persona {
     }
 
     //Constructor con todos los atributos
-    public Persona(String nombre, int edad, String sexo, int altura, int peso, int dni) {
+    public Persona(String nombre, int edad, String sexo, int  peso, int altura) {
 
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
         this.altura = altura;
         this.peso = peso;
-        this.dni = dni;
+    
 
     }
 
 
     // calcular peso ideal
-    public void calcularIMC() {
+  /*  public int calcularIMC(int altura, int peso) {
 
         int pesoIdeal = (peso)/(altura*altura);
 
         if (pesoIdeal < 20) {
 
-            resultadoPeso = -1;
+            pesoIdeal = -1;
+            return pesoIdeal;
             
         }
 
         if (pesoIdeal >= 20 && pesoIdeal <=25) {
 
             resultadoPeso =0;
+
+            return pesoIdeal;
             
         }
 
         if (pesoIdeal>25) {
 
             resultadoPeso = 1;
+            return pesoIdeal;
             
         }
 
-    
+        public boolean EsMayorDeEdad(int edad){
 
+        if (edad > 18) {
+
+            mayorEdad = true;
+
+            return mayorEdad;
+            
+        }
+
+        else{
+
+            mayorEdad = false;
+            return mayorEdad;
+        }
+
+    }
+
+     public char ComprobarSexo(char sexo){
+
+
+        if (char == m) {
+
+            sexo = mujer;
+            return sexo;
+
+            
+        }
+
+        else{
+
+            sexo = h;
+            return sexo;
+        }
+
+
+
+    }
+
+    public void generarDni(){
+
+     dni = (int) (Math.random() * (1000000 - 1000000 ) +1 );
+
+     System.out.println("dni");
+
+    } */
+
+    public void mostrarDAtos(Persona persona){
+
+        System.out.println(persona.toString());
+
+
+
+    }
+    
 }
